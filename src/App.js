ddf36676d22f3a31645d2c057dfb6574b523cbd9
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-23 10:58:21 +0100

    add Routes & Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home/Home";
+import SinglePost from "./components/pages/Post/SinglePost/SinglePost";
+import About from "./components/pages/About/About";
+import PostAdd from "./components/pages/Post/PostAdd/PostAdd";
+import PageNotFound from "./components/pages/NotFound/NotFound";
+import EditPost from "./components/pages/Post/EditPost/EditPost";
 
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<SinglePost />} />
+        <Route path="/post/add" element={<PostAdd />} />
+        <Route path="/post/edit/:id" element={<EditPost />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} /> 
+      </Routes>
+    </main>
   );
 };
 

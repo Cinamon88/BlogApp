36685442b12f313749e2ca189acc1019d645ea7e
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-23 13:56:46 +0100

    remove import PageTitle

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,9 +1,7 @@
-// import PageTitle from '../PageTitle/PageTitle';
-
 const About = () => {
     return (
         <div>
-            <h1>ABOUT </h1>
+            <h1>ABOUT</h1>
         </div>
     )
 }

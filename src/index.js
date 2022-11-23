e68e46a670c182691a35c7cd8126e3d3e7d02854
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-23 10:57:35 +0100

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.querySelector('#root')
 );

ad5fc083644ca3dbed1681dca0864c97d957bbc2
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-26 21:02:42 +0100

    add selector getPostById

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,6 +1,8 @@
 
 //selectors
 export const getAllPosts = (state) => state.posts;
+export const getPostById = ({ posts }, id) =>
+  posts.find((post) => post.id === id);
 
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
@@ -8,6 +10,7 @@ const ADD_POST = createActionName('ADD_POST');
 
 // action creators
 export const addPost = (payload) => ({ type: ADD_POST, payload });
+
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
     case ADD_POST:

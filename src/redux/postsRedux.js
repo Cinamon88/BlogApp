b197043696c73b5505db3b4852c09a24d5152ca2
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-29 15:08:54 +0100

    add action & creator REMOVE_POST

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -7,14 +7,18 @@ export const getPostById = ({ posts }, postId) =>
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
 const ADD_POST = createActionName('ADD_POST');
+const REMOVE_POST = createActionName('REMOVE_POST');
 
 // action creators
 export const addPost = (payload) => ({ type: ADD_POST, payload });
+export const removePost = (payload) => ({ type: REMOVE_POST, payload});
 
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
     case ADD_POST:
         return [...statePart, { ...action.payload }];
+    case REMOVE_POST:
+        return statePart.filter((post) => post.id !== action.payload);
     default:
       return statePart;
   };

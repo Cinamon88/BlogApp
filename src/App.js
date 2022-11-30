ae6c6d7b7851ef532e7a1cb23d92480ab06af798
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-30 06:10:37 +0100

    modify routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import SinglePost from "./components/pages/Post/SinglePost/SinglePost";
 import About from "./components/pages/About/About";
-import PostAdd from "./components/pages/Post/PostAdd/PostAdd";
 import PageNotFound from "./components/pages/NotFound/NotFound";
-import EditPost from "./components/pages/Post/EditPost/EditPost";
 import { Container } from 'react-bootstrap'
 import Header from "./components/views/Header";
 import Footer from "./components/views/Footer";
+import AddPostForm from "./components/features/AddPostForm";
+import EditPostForm from "./components/features/EditPostForm";
 
 const App = () => {
   return (
@@ -16,8 +16,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<SinglePost />} />
-        <Route path="/post/add" element={<PostAdd />} />
-        <Route path="/post/edit/:id" element={<EditPost />} />
+        <Route path="/post/add" element={<AddPostForm />} />
+        <Route path="/post/edit/:id" element={<EditPostForm />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<PageNotFound />} /> 
       </Routes>

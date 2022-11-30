55d53ead9ede2724aef758304114e2cf7fa256cc
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-30 06:09:38 +0100

    add EDIT_POST action & creator

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -8,10 +8,12 @@ export const getPostById = ({ posts }, id) =>
 const createActionName = actionName => `app/posts/${actionName}`;
 const ADD_POST = createActionName('ADD_POST');
 const REMOVE_POST = createActionName('REMOVE_POST');
+const EDIT_POST = createActionName("EDIT_POST");
 
 // action creators
 export const addPost = (payload) => ({ type: ADD_POST, payload });
 export const removePost = (id) => ({ type: REMOVE_POST, id});
+export const editPost = (payload) => ({ type: EDIT_POST, payload});
 
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
@@ -19,6 +21,9 @@ const postsReducer = (statePart = [], action) => {
         return [...statePart, { ...action.payload }];
     case REMOVE_POST:
         return statePart.filter((post) => post.id !== action.id);
+    case EDIT_POST:
+        return statePart.map(post => (post.id === action.payload.id ? 
+               { ...post, ...action.payload } : post));
     default:
       return statePart;
   };

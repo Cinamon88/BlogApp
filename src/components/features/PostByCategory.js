3998b396b64ea2daccee45cd09eed6d4b58901f7
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-12-03 17:34:13 +0100

    add new component

---
+++ src/components/features/PostByCategory.js
@@ -0,0 +1,37 @@
+import { Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getPostByCategory } from '../../redux/postsRedux';
+import PostContainer from './PostContainer';
+
+
+const PostByCategory = () => {
+  const { postCategory } = useParams();
+  const posts = useSelector((state) => getPostByCategory(state, postCategory));
+
+  if (posts.length === 0)
+    return (
+      <div>
+        <h1>Category: {postCategory}</h1>
+        <p>No posts in this category</p>
+      </div>
+    );
+  return (
+    <div>
+      <Row xs={1} md={4} className="g-2">
+        <Col col={12}>
+          <h1>Category: {postCategory}</h1>
+        </Col>
+      </Row>
+      <Row xs={1} md={2} className="g-3 justify-content-md-center">
+        {posts.map((post) => (
+          <Col key={post.id}>
+            <PostContainer {...post} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default PostByCategory;

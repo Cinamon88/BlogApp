2215f1d0b153f776f3014c2ba53c7203a14bb136
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-12-04 19:39:49 +0100

    fix selector

--- src/components/features/PostByCategory.js
+++ src/components/features/PostByCategory.js
@@ -1,40 +1,46 @@
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getPostByCategory } from '../../redux/postsRedux';
-import PostCard from './PostCard';
-
+import { dateToStr } from '../../utils/dateToStr';
+import { Link } from 'react-router-dom';
 
 const PostByCategory = () => {
   const { postCategory } = useParams();
-  const posts = useSelector(getPostByCategory);
+  const posts = useSelector((state) => getPostByCategory(state, postCategory));
 
   if (posts.length === 0)
     return (
-      <div>
+      <>
         <h1>Category: {postCategory}</h1>
-        <p>No posts in this category</p>
-      </div>
+        <h3>No posts in this category</h3>
+      </>
     );
-
   return (
-    <div>
-      <Row xs={1} md={4} className="g-2">
-        <Col col={12}>
-          <h1>Category: {postCategory}</h1>
-        </Col>
-      </Row>
-      <Row xs={1} md={2} className="g-3 justify-content-md-center">
-      {posts.map((post, index) => (
-          <div key={index}>
-            <PostCard
-              key={index}
-              id={post.id}
-              />
-          </div>
+    <>
+      <h1>Category: {postCategory}</h1>
+      <Row>
+        {posts.map((post) => (
+          <Col key={post.id} xs={12} sm={6} lg={4}>
+            <Card key={post.id} className='mt-4'>
+              <Card.Body>
+                <Card.Title>{post.title}</Card.Title>
+                <Card.Text>
+                  <strong>Author:</strong> {post.author} <br />
+                  <strong>Published:</strong> {dateToStr(post.publishedDate)}
+                  <br />
+                  <strong>Category:</strong> {post.category}
+                </Card.Text>
+                <Card.Text>{post.shortDescription}</Card.Text>
+                <Button variant='primary' as={Link} to={'/post/' + post.id}>
+                  Read more
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
       </Row>
-    </div>
+    </>
   );
 };
 
